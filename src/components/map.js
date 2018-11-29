a54aa0a8f4f4f1bba9db9ebe5c07d34f749694b2
KJF <kjf.artwork@gmail.com>
2018-11-29 14:13:25 -0700

    Map slc position and marker

--- src/components/map.js
+++ src/components/map.js
@@ -2,50 +2,32 @@ import React, { Component } from "react";
 import { Map, InfoWindow, Marker, GoogleApiWrapper } from "google-maps-react";
 // import * as fourSquareAPI from './APIs/fourSquareAPI.js';
 
-export class MapContainer extends Component {
+class MapContainer extends Component {
+  //  Bounds: 40.544379,-111.783164 — 40.442562,-111.921768
+
   render() {
-    return <Map google={this.props.google} zoom={14} />;
+    return (
+      <Map
+        google={this.props.google}
+        className={"map"}
+        style={{ width: "100%", height: "100%" }}
+        initialCenter={{
+          lat: 40.524671,
+          lng: -111.863823
+        }}
+        zoom={12}
+      >
+        <Marker
+          name={"Draper Park"}
+          position={{ lat: 40.523436, lng: -111.85459 }}
+        />
+      </Map>
+    );
   }
 }
 
 export default GoogleApiWrapper({
   apiKey: "AIzaSyBYX4tZYpMWfZQ_kdub-M4ONYJFKQTp6Ac"
 })(MapContainer);
-
-// import React, { Component } from "react";
-// import GoogleMapReact from "google-map-react";
-
-// const AnyReactComponent = ({ text }) => <div>{text}</div>;
-
-// class Map extends Component {
-//   static defaultProps = {
-//     center: {
-//       lat: 40.524671,
-//       lng: -111.863823
-//     },
-//     zoom: 11
-//   };
-
-//   render() {
-//     return (
-//       // Important! Always set the container height explicitly
-//       <div style={{ height: "100vh", width: "100%" }}>
-//         <GoogleMapReact
-//           bootstrapURLKeys={{ key: AIzaSyBYX4tZYpMWfZQ_kdub - M4ONYJFKQTp6Ac }}
-//           defaultCenter={this.props.center}
-//           defaultZoom={this.props.zoom}
-//         >
-//           <AnyReactComponent
-//             lat={40.457842}
-//             lng={-111.901385}
-//             text={"Kreyser Avrora"}
-//           />
-//         </GoogleMapReact>
-//       </div>
-//     );
-//   }
-// }
-
-// export default Map;
 
 // // https://maps.googleapis.com/maps/api/js?key=AIzaSyBYX4tZYpMWfZQ_kdub-M4ONYJFKQTp6Ac&callback=initMap
